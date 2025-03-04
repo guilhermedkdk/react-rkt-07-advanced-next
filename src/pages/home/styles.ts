ac4746d87bf88f2f118165b9d4e33c0b13baf3c7
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-03-04 17:22:47 -0300

    feat: add username form and its validation with zod

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,26 +1,25 @@
 import { styled, Heading, Text } from '@ignite-ui/react'
 
 export const Container = styled('div', {
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
+  marginLeft: 'auto',
+  height: '100vh',
   display: 'flex',
   alignItems: 'center',
-
-  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
-  height: '100vh',
-  marginLeft: 'auto',
   gap: '$20',
 })
 
 export const Hero = styled('div', {
   maxWidth: 480,
-  paddingTop: '0 $10',
+  padding: '0 $10',
 
-  [`${Heading}`]: {
+  [`> ${Heading}`]: {
     '@media (max-width: 600px)': {
       fontSize: '$6xl',
     },
   },
 
-  [`${Text}`]: {
+  [`> ${Text}`]: {
     marginTop: '$2',
     color: '$gray200',
   },
