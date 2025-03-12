e4a57e7548c9d443779dcd1a3bf8ab3a6ebf495d
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-03-12 16:07:28 -0300

    feat: add calendar and time picker visual structure

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,27 @@
+import { Calendar } from '@/components/Calendar'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
+
+export function CalendarStep() {
+  const isDateSelected = true
+
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader></TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem></TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  )
+}
