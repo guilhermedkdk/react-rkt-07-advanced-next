4e63e70256f68e6f255217bf067e53b29bbd5425
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-03-16 14:27:18 -0300

    fix: add eslint disables for deploy

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useMemo, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { useRouter } from 'next/router'
